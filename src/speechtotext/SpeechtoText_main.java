3056226ca110c4a9110bd4d5dc33c5be2b50fad5
mizuno1xx <43839232+mizuno1xx@users.noreply.github.com>
2018-10-25 14:29:43 +0900

    Add files via upload

--- src/speechtotext/SpeechtoText_main.java
+++ src/speechtotext/SpeechtoText_main.java
@@ -2,20 +2,22 @@ package speechtotext;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechRecognitionResults;
 
 public class SpeechtoText_main 
 {
-
 	public static void main(String[] args) 
 	{
 		SpeechToText service = new SpeechToText();
 	    service.setUsernameAndPassword("1618101", "1618101");
 
-	    File audio = new File("audio/sample1.wav");
+	    File audio = new File("audio/1.wav");
 	    RecognizeOptions options = null;
 		try
 		{
@@ -24,6 +26,7 @@ public class SpeechtoText_main
 			    .audio(audio)
 			    .contentType(RecognizeOptions.ContentType.AUDIO_WAV)
 			    .build();
+
 		} 
 		catch (FileNotFoundException e) 
 		{
@@ -31,7 +34,26 @@ public class SpeechtoText_main
 		}
 	    SpeechRecognitionResults transcript = service.recognize(options).execute();
 
-	    System.out.println(transcript);		
-	}
+	    System.out.println(transcript);	
+	    String s = String.valueOf(transcript); 
+	    ObjectMapper mapper = new ObjectMapper();
+	    
+	    try 
+	    {
+			JsonNode node = mapper.readTree(s);
+			
+			for( int i = 0 ; i < node.get("results").size() ; i ++ )
+			{
+				String text = node.get("results").get(i).get("alternatives").get(0).get("transcript").toString();
+				double confidence = node.get("results").get(i).get("alternatives").get(0).get("confidence").asDouble();
+				System.out.println( "transcript:" + text );
+				System.out.println( "confidence:" + confidence );
+			}
+		} 
+	    catch (IOException e) 
+	    {
+			e.printStackTrace();
+		}
 
+	}
 }
