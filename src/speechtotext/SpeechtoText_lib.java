2c88d502df0a432389b9c2f4ee5436ceece399cf
mizuno1xx <43839232+mizuno1xx@users.noreply.github.com>
2018-10-25 15:24:02 +0900

    Add files via upload

---
+++ src/speechtotext/SpeechtoText_lib.java
@@ -0,0 +1,65 @@
+package speechtotext;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechRecognitionResults;
+
+public class SpeechtoText_lib 
+{
+	SpeechToText service;
+    RecognizeOptions options = null;
+    
+	public SpeechtoText_lib( File audio ) 
+	{
+		service = new SpeechToText();
+		service.setUsernameAndPassword("1618101", "1618101");
+		
+		try
+		{
+			options = new RecognizeOptions.Builder()
+				.model("ja-JP_BroadbandModel")
+			    .audio(audio)
+			    .contentType(RecognizeOptions.ContentType.AUDIO_WAV)
+			    .build();
+		} 
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+    
+	public SpeechRecognitionResults getTranscript() 
+	{
+		SpeechRecognitionResults transcript = service.recognize(options).execute();
+		return transcript;
+	}
+	
+	public void GetJson( SpeechRecognitionResults transcript )
+	{
+		String s = String.valueOf(transcript);
+	    ObjectMapper mapper = new ObjectMapper();
+	    
+	    try 
+	    {
+			JsonNode node = mapper.readTree(s);
+			
+			for( int i = 0 ; i < node.get("results").size() ; i ++ )
+			{
+				String text = node.get("results").get(i).get("alternatives").get(0).get("transcript").toString();
+				double confidence = node.get("results").get(i).get("alternatives").get(0).get("confidence").asDouble();
+				System.out.println( "transcript:" + text );
+				System.out.println( "confidence:" + confidence );
+			}
+		} 
+	    catch (IOException e) 
+	    {
+			e.printStackTrace();
+		}	
+	}
+}
--- src/speechtotext/SpeechtoText_main.java
+++ src/speechtotext/SpeechtoText_main.java
@@ -1,59 +1,21 @@
 package speechtotext;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
-import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechRecognitionResults;
 
 public class SpeechtoText_main 
 {
 	public static void main(String[] args) 
 	{
-		SpeechToText service = new SpeechToText();
-	    service.setUsernameAndPassword("1618101", "1618101");
+		
+		SpeechtoText_lib slib = new SpeechtoText_lib( new File( "audio/1.wav" ) );
+		SpeechRecognitionResults transcript = slib.getTranscript();
+		System.out.println(transcript);
 
-	    File audio = new File("audio/1.wav");
-	    RecognizeOptions options = null;
-		try
-		{
-			options = new RecognizeOptions.Builder()
-				.model("ja-JP_BroadbandModel")
-			    .audio(audio)
-			    .contentType(RecognizeOptions.ContentType.AUDIO_WAV)
-			    .build();
-
-		} 
-		catch (FileNotFoundException e) 
-		{
-			e.printStackTrace();
-		}
-	    SpeechRecognitionResults transcript = service.recognize(options).execute();
-
-	    System.out.println(transcript);	
-	    String s = String.valueOf(transcript); 
-	    ObjectMapper mapper = new ObjectMapper();
-	    
-	    try 
-	    {
-			JsonNode node = mapper.readTree(s);
-			
-			for( int i = 0 ; i < node.get("results").size() ; i ++ )
-			{
-				String text = node.get("results").get(i).get("alternatives").get(0).get("transcript").toString();
-				double confidence = node.get("results").get(i).get("alternatives").get(0).get("confidence").asDouble();
-				System.out.println( "transcript:" + text );
-				System.out.println( "confidence:" + confidence );
-			}
-		} 
-	    catch (IOException e) 
-	    {
-			e.printStackTrace();
-		}
-
+		slib.GetJson( transcript );
 	}
 }
